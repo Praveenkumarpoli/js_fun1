5d6b21ae2cea633f98a30ea2b67760654b312f03
PoliPavan <polipavan123@gmail.com>
2024-09-12 01:01:11 +0530

    js_fun1

---
+++ script.js
@@ -0,0 +1,95 @@
+
+// task1
+
+function showMessage() {
+    console.log("Hello, welcome to the website!");
+    }
+    showMessage();
+    
+    // task2
+    function countToTen() {
+        for (let i = 1; i <= 10; i++) {
+            console.log(i);
+        }
+    }
+    countToTen();
+    
+    // task3
+    
+    function multiplicationTable() {
+        const number = 2; 
+        for (let i = 1; i <= 10; i++) {
+            console.log(`${number} x ${i} = ${number * i}`);
+        }
+    }
+    multiplicationTable();
+    
+    
+    // task4
+    
+    
+    function showOddNumbers() {
+        for (let i = 1; i <= 10; i++) {
+            if (i % 2 !== 0) {
+                console.log(i);
+            }
+        }
+    }
+    showOddNumbers();
+    
+    // task5
+    
+    function countDown() {
+        for (let i = 5; i >= 1; i--) {
+            console.log(i);
+        }
+    }
+    countDown();
+    
+    
+    // task6
+    
+    function greetUser() {
+        console.log("Have a nice day brother!");
+    }
+    greetUser();
+    
+    // task7
+    
+    function showEvenNumbers() { 
+        for (let i = 1; i <= 10; i++) {
+            if (i % 2 === 0) {
+                console.log(i);
+            }
+        }
+    }
+    showEvenNumbers();
+    
+    // task8
+    
+    function farewellMessage() {
+        console.log("Thank you for visiting, goodbye!");
+    }
+    farewellMessage();
+    
+    // task9
+    
+    function showSquares() {
+        for (let i = 1; i <= 5; i++) {
+            let square = i * i;
+            console.log(`The square of ${i} is ${square}`);
+        }
+    }
+    showSquares();
+    
+    // task10
+    
+    function repeatWelcome() {
+        for (let i = 0; i < 3; i++) {
+            console.log("Welcome!");
+        }
+    }
+    repeatWelcome();
+    
+    
+    
